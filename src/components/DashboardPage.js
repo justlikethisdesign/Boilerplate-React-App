650d0bf53e4a400f1647bb625cc7c6996fe1c481
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2018-12-06 08:13:46 +0000

    Cleaned up boilerplate

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import ExpenseList from './ExpenseList';
-import ExpenseListFilters from './ExpenseListFilters';
-import ExpensesSummary from './ExpensesSummary';
 
 const ExpenseDashboardPage = () => (
   <div>
-    <ExpensesSummary />
-    <ExpenseListFilters />
-    <ExpenseList />
+    dashboard content
   </div>
 );
 
